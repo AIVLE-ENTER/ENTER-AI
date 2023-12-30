bd44bd8600cd8b7d50ab79075954b401b54ad513
asd <asf>
2023-12-30 15:31:52 +0900

    Fix: 🎨 allow CORS

--- project/main.py
+++ project/main.py
@@ -1,13 +1,29 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from server.apps import FastApiServer
+
+origins = [
+    "http://localhost:5501",
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
+    "http://127.0.0.1:5501",
+]
 
 def main():
     app = FastAPI(
         title="LangChain Server",
         version="1.0",
         description="A simple api server using Langchain's Runnable interfaces",
+    )
+    
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
     )
 
     server = FastApiServer()
